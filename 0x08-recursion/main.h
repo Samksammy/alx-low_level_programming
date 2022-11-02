8bd177fc036a1ec68b43f9ecffade7c14d1ad17a
“Samksammy” <samuelkwabe@gmail.com>
2022-11-02 15:10:16 -0700

    0x08-recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar (char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
